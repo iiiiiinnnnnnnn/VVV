cfe0480f271bf119add37b844e0e72c41fffe657
iiiiiinnnnnnnn <takedalryo@gmail.com>
2026-05-18 13:45:11 +0900

    point light and spot light

--- Source/Light.h
+++ Source/Light.h
@@ -8,15 +8,61 @@ struct DirectionalLight
 	Vector3	color = { 1, 1, 1 };
 };
 
+struct PointLight
+{
+	Vector3	position = { 0, 0, 0 };
+	Vector3	color = { 1, 1, 1 };
+	float	range = 10.0f;
+};
+
+struct SpotLight
+{
+	Vector3	position = { 0, 0, 0 };
+	Vector3	direction = { 0, -1, 0 };
+	Vector3	color = { 1, 1, 1 };
+	float	range = 10.0f;
+	float	innerConeAngle = DirectX::XMConvertToRadians(15.0f);
+	float	outerConeAngle = DirectX::XMConvertToRadians(30.0f);
+};
+
 class LightManager
 {
 public:
+	constexpr static int MaxPointLights = 4;
+	constexpr static int MaxSpotLights = 4;
+
 	// ディレクショナルライト設定
 	void SetDirectionalLight(DirectionalLight& light) { directionalLight = light; }
 
 	// ディレクショナルライト取得
 	const DirectionalLight& GetDirectionalLight() const { return directionalLight; }
 
+	// ポイントライト追加
+	bool AddPointLight(PointLight& light)
+	{
+		if (pointLightCount >= MaxPointLights) return false;
+		pointLights[pointLightCount++] = light;
+		return true;
+	}
+
+	// ポイントライト取得
+	const PointLight* GetPointLights() const { return pointLights; }
+
+	// スポットライト追加
+	bool AddSpotLight(SpotLight& light)
+	{
+		if (spotLightCount >= MaxSpotLights) return false;
+		spotLights[spotLightCount++] = light;
+		return true;
+	}
+
+	// スポットライト取得
+	const SpotLight* GetSpotLights() const { return spotLights; }
+
 private:
 	DirectionalLight	directionalLight;
+	PointLight			pointLights[MaxPointLights];
+	SpotLight			spotLights[MaxSpotLights];
+	unsigned int 		pointLightCount = 0;
+	unsigned int 		spotLightCount = 0;
 };
