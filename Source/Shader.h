f6a7c05276d6066931c51c052c220a6bf83d4010
iiiiiinnnnnnnn <takedalryo@gmail.com>
2026-05-20 11:42:22 +0900

    j

--- Source/Shader.h
+++ Source/Shader.h
@@ -4,16 +4,12 @@
 #include "RenderContext.h"
 #include "Model.h"
 
-class SpriteShader
-{
-public:
-	SpriteShader() {}
-	virtual ~SpriteShader() {}
+class Shader {
 
+public:
 	virtual void Begin(const RenderContext& rc) = 0;
-	virtual void Update(const RenderContext& rc, ID3D11ShaderResourceView* srv, float r, float g, float b, float a, float elapsedTime) = 0;
-	virtual void ApplyParams(std::shared_ptr<void> params) {}
 	virtual void End(const RenderContext& rc) = 0;
+	virtual void ApplyParams(std::shared_ptr<void> params) {}
 
 protected:
 	Microsoft::WRL::ComPtr<ID3D11VertexShader>		vertexShader;
@@ -23,20 +19,14 @@ protected:
 	static const std::vector<D3D11_INPUT_ELEMENT_DESC> InputElementDescs;
 };
 
-class ModelShader
+class SpriteShader : public Shader
 {
 public:
-	ModelShader() {}
-	virtual ~ModelShader() {}
+	virtual void Update(const RenderContext& rc, ID3D11ShaderResourceView* srv, float r, float g, float b, float a, float elapsedTime) = 0;
+};
 
-	virtual void Begin(const RenderContext& rc) = 0;
+class ModelShader : public Shader
+{
+public:
 	virtual void Update(const RenderContext& rc, const Model::Mesh& mesh, float elapsedTime) = 0;
-	virtual void End(const RenderContext& rc) = 0;
-
-protected:
-	Microsoft::WRL::ComPtr<ID3D11VertexShader>		vertexShader;
-	Microsoft::WRL::ComPtr<ID3D11PixelShader>		pixelShader;
-	Microsoft::WRL::ComPtr<ID3D11InputLayout>		inputLayout;
-
-	static const std::vector<D3D11_INPUT_ELEMENT_DESC> InputElementDescs;
 };
