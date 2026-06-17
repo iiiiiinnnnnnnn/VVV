63e5aaa8ef61bad1f675c61bc67727978561ed12
iiiiiinnnnnnnn <takedalryo@gmail.com>
2026-06-17 15:25:42 +0900

    idt

--- Source/Stage01.cpp
+++ Source/Stage01.cpp
@@ -4,20 +4,11 @@
 
 Stage01::Stage01() : Actor("Stage01", "Stage", Layer::Stage)
 {
-    auto* rb = AddComponent<RigidbodyStatic>();
+	auto* rb = AddComponent<RigidbodyStatic>();
 
-    Terrain* terrain = AddComponent<Terrain>();
+	Terrain* terrain = AddComponent<Terrain>();
 	terrain->LoadTerrainTexture("Data/Terrain/Maps/BossField.dds");
-	AddComponent<TerrainMeshCollider>(
-		rb,
-		256,
-		TerrainMeshCollider::CollisionArea
-		{
-			0.2844f,
-			0.7242f,
-			0.2438f,
-			0.6844f
-		});
+	AddComponent<TerrainMeshCollider>(rb, 256, TerrainMeshCollider::CollisionArea{0.2844f, 0.7242f, 0.2438f, 0.6844f});
 }
 
 void Stage01::OnUpdate()
